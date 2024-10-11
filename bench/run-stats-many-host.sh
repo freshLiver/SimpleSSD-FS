b327ea38ff0ec9d8b90dd3c8d29487dbd6e6a27c
Yuanhsi Chung <freshliver.cys@gmail.com>
2024-10-11 19:32:07 +0800

    Bench: add stats32/64 bench scripts

---
+++ bench/run-stats-many-host.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ssfx="$1"
+MAKE_ARGS_EX="$2"
+if [[ -z "$ssfx" ]]; then
+    echo "Usage: $0 sub-suffix [MAKE_ARGS_EX]"
+    exit 1
+fi
+
+read WORKNAME WORKTYPE MODE64 <<< "stats host-o2 -mode64"
+
+workloads=(
+    # "/$WORKNAME/x500/ x500"
+    # "/$WORKNAME/x1000/ x1000"
+    # "/$WORKNAME/x1500/ x1500"
+    "/$WORKNAME/x2000/ x2000"
+)
+
+for work in "${workloads[@]}"; do
+    read path sfx <<< "$work"
+
+    TMP_FILE=$(mktemp -t gem5-$WORKNAME-$WORKTYPE-$sfx.XXXXXXXX)
+
+    echo "
+    #!/bin/bash
+    mount /dev/sdb /mnt
+    mount /dev/nvme0n1 /nvme
+    /mnt/$WORKNAME-$WORKTYPE --path /nvme$path $MODE64
+    m5 exit" > "$TMP_FILE"
+
+    cat "$TMP_FILE"
+
+    MAKE_ARGS="M5_LOG_SUFFIX=$sfx-$ssfx TIME=$(date +%y%m%d-%H%M%S) $MAKE_ARGS_EX"
+    make run-timing GEM5_SCRIPT=$TMP_FILE $MAKE_ARGS &> /dev/null &
+    sleep 5
+    make socat-background $MAKE_ARGS &
+
+    # wait both gem5 and host
+    echo "Workload ($MAKE_ARGS) is running, wait for gem5 and host..."
+    jobs
+    wait $(jobs -p)
+done
