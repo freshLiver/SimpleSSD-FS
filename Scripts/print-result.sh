5d2fc8b32dfdbdf5dae99e6de8a2e24c06522faa
Yuanhsi Chung <freshliver.cys@gmail.com>
2024-11-04 07:00:20 +0800

    Scripts: update monitor script

--- Scripts/print-result.sh
+++ Scripts/print-result.sh
@@ -5,11 +5,13 @@ DIR=${1:-"logs"}
 HOST_LOGS="$DIR/*.host.log"
 DEBUG_LOGS="$DIR/*.log"
 
-simtime="$(grep -ir 'Simulation Time' $HOST_LOGS)"
+simtime="$(grep -Er '(Simu|Comp|Sleep).* Time' $HOST_LOGS)"
 iscpath="$(grep -m 3 -oE 'EXT4: lookup: '.*'' $DEBUG_LOGS)"
 
 sort <(echo "$simtime") <(echo "$iscpath") \
-    | sed 's/host.log:/host.log \t:/g' \
+    | sed "s|^.*/||g" \
+    | sed -E 's/(\.host)?\.log:/\t/g' \
     | sed 's/~/ /g' \
-    | sed 's/:EXT4: lookup:/     \t: /g' \
-    | grep -P --color=always "(\d+ \d+|'(/[\w\.]+)+')"
+    | sed 's/:EXT4: lookup:/     \t /g' \
+    | grep -P --color=always "(\d+ +ps|\d+ \d+|\d+\.\d+|'(/[\w\.]+)+')" \
+    | column -t
